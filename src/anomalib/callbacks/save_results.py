9a0aeef51cc05924262bbedab97e068da4662adb
slutzky <slutzky@aligntech.com>
2024-12-30 15:19:57 +0200

    fix cpu list

--- src/anomalib/callbacks/save_results.py
+++ src/anomalib/callbacks/save_results.py
@@ -60,7 +60,7 @@ class SaveResults(Callback):
                 'pred_boxes': [box.cpu().numpy() for box in outputs['pred_boxes']],
                 'box_scores': [score.cpu().numpy() for score in outputs['box_scores']],
                 'box_labels': [label.cpu().numpy() for label in outputs['box_labels']],
-                'image_path':outputs['pred_scores']
+                'image_path':outputs['image_path']
             }
             pickle.dump(data_dict, file)
 
